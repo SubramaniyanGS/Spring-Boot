f537d1aeb9fb883ba96d820f0068b5fe4a1f492d
Subramaniyan <subugksd2002@gmail.com>
2024-06-15 13:45:37 +0530

    initial Commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public User signup(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.loginUser(user.getEmail(), user.getPassword());
+    }
+}
